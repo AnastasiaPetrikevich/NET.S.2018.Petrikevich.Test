e66eb184cb1904d930913755667f5364c28976da
Петрикевич Анастасия <nastya.petrikevich@gmail.com>
2018-04-23 09:37:32 +0300

    Add project files.

---
+++ Task2.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.Solution;
+
+namespace Task2.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomFileGenerator charFile = new RandomCharsFileGenerator("Files with random chars", ".txt");
+            charFile.GenerateFiles(1, 100);
+            RandomFileGenerator bytesFile = new RandomBytesFileGenerator("Files with random bytes", ".bytes");
+            bytesFile.GenerateFiles(1, 100);
+        }
+    }
+}
---
+++ Task2.Solution/RandomBytesFileGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Solution
+{
+    public class RandomBytesFileGenerator : RandomFileGenerator
+    {
+        public RandomBytesFileGenerator(string WorkingDirectory, string FileExtension):base(WorkingDirectory,FileExtension)
+        { }
+
+        internal override byte[] GenerateFileContent(int contentLength)
+        {
+            var random = new Random();
+
+            var fileContent = new byte[contentLength];
+
+            random.NextBytes(fileContent);
+
+            return fileContent;
+        }
+    }
+}
